2fed4b7a078ac30a1ca66269b768389758aadf10
Masibulele22 <sticky.ngondo@gmail.com>
2021-04-19 11:38:55 +0200

    changes 3

---
+++ main.py
@@ -0,0 +1,14 @@
+    # Determining if someone qualifies for a loan.
+
+
+
+    salary_per_annum = float(input("Enter  your salary per annum: "))
+    employment_period = int(input('Enter employment period in years: '))
+
+    if salary_per_annum >= 30000:
+        if employment_period > 2:
+            print("You qualify for a loan.")
+    else:
+        print("You do not qualify for a loan.")
+
+
